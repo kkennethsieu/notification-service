0c0091e29087d436db8c47b0dfe10089b1e0a58c
kkennethsieu <kkennethsieu@gmail.com>
2025-12-16 20:03:33 -0800

    Updated Ci

--- src/api/getGame.js
+++ src/api/getGame.js
@@ -1,4 +1,4 @@
-const gameUrl = process.env.GAME_SERVICE_URL || "http://localhost:3002";
+const gameUrl = process.env.CATALOG_SERVICE_URL || "http://localhost:3002";
 export const getGame = async (gameId) => {
   const res = await fetch(`${gameUrl}/games/id/${gameId}`);
   if (!res.ok) throw new Error("Erro fetching game");
